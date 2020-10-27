44572984046eb7247b795e0ef50c16677f2433e1
varun kumar <71749736+va-run04@users.noreply.github.com>
2020-10-27 19:42:43 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int marks;
+printf("enter your marks:");
+scanf("%d",&marks);
+if(marks>=85&&marks<=100)
+	{
+		printf("Grade A");
+	}
+	else if(marks>=70&&marks<=84)
+	{
+		printf("Grade B");
+	}
+	else if(marks>=55&&marks<=69)
+	{
+			printf("Grade C");
+	}
+	else if(marks>=40&&marks<=54)
+	{
+       printf("Grade D");	
+	}
+	else if(marks<40)
+	{
+	printf("Grade F");		
+	}
+	 return 0;
+	
+}
